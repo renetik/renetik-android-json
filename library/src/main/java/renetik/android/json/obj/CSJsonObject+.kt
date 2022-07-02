b4e6aec9c25af648748842f55d92c88208cdabd3
Rene Dohan <dohan.rene@gmail.com>
2022-07-02 14:08:15 -0500

    Update CSJsonObject+.kt

--- library/src/main/java/renetik/android/json/obj/CSJsonObject+.kt
+++ library/src/main/java/renetik/android/json/obj/CSJsonObject+.kt
@@ -7,7 +7,6 @@ import renetik.android.json.parseJsonMap
 import renetik.android.json.toJSONObject
 import java.io.File
 
-
 fun CSJsonObject.toJsonObject() = data.toJSONObject()
 
 fun CSJsonObject(data: String) = CSJsonObject().load(data)
@@ -17,9 +16,9 @@ fun <T : CSJsonObject> T.load(data: String) = apply { load(data.parseJsonMap()!!
 fun <T : CSJsonObject> T.load(file: File) = load(file.readText())
 
 fun <T : CSJsonObject> T.loadAsset(file: String) =
-	load(app.assets.open(file).readText())
+    load(app.assets.open(file).readText())
 
 fun CSJsonObject.getObject(key: String) = getMap(key)?.let(CSJsonObject()::load)
 
 fun <T : CSJsonObject> T.clone() =
-	this::class.createInstance()!!.also { it.load(this.data) }
+    this::class.createInstance()!!.also { it.load(this.data) }
