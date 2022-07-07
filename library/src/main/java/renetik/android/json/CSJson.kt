8e65160fe4ebec1d837c0f26c04c7d7801466e50
Rene Dohan <dohan.rene@gmail.com>
2022-07-06 23:16:45 -0500

    Update CSJson.kt

--- library/src/main/java/renetik/android/json/CSJson.kt
+++ library/src/main/java/renetik/android/json/CSJson.kt
@@ -1,11 +1,6 @@
 package renetik.android.json
 
-import renetik.android.core.lang.CSEnvironment.isDebug
-
 object CSJson {
     var forceStringInJson = false
-    var isJsonFormatted = isDebug
-    fun disableJsonFormat() {
-        isJsonFormatted = false
-    }
+    var isJsonFormatted = false
 }
