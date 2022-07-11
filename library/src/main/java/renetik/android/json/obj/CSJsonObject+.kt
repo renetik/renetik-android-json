e2d29f083f8bf8741a6d429b9e08afd0efa4187f
Rene Dohan <dohan.rene@gmail.com>
2022-07-11 18:17:02 -0500

    updateModuleToGithub

--- library/src/main/java/renetik/android/json/obj/CSJsonObject+.kt
+++ library/src/main/java/renetik/android/json/obj/CSJsonObject+.kt
@@ -1,8 +1,8 @@
 package renetik.android.json.obj
 
-import renetik.android.core.CSApplication.Companion.app
 import renetik.android.core.java.io.readText
 import renetik.android.core.kotlin.reflect.createInstance
+import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.json.parseJsonMap
 import renetik.android.json.toJSONObject
 import java.io.File
@@ -15,8 +15,7 @@ fun <T : CSJsonObject> T.load(json: String) = apply { load(json.parseJsonMap()!!
 
 fun <T : CSJsonObject> T.load(file: File) = load(file.readText())
 
-fun <T : CSJsonObject> T.loadAsset(file: String) =
-    load(app.assets.open(file).readText())
+fun <T : CSJsonObject> T.loadAsset(file: String) = load(app.assets.open(file).readText())
 
 fun CSJsonObject.getObject(key: String) = getMap(key)?.let(CSJsonObject()::load)
 
